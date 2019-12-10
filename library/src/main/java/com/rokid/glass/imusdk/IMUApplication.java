7b6357763ee777a1908631ea68e050d584f10f94
yeungeek <yangjian0410@gmail.com>
2019-12-10 14:58:43 +0800

    [ADD] init imu library

---
+++ library/src/main/java/com/rokid/glass/imusdk/IMUApplication.java
@@ -0,0 +1,10 @@
+package com.rokid.glass.imusdk;
+
+import android.app.Application;
+
+public class IMUApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
